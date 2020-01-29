80ba10887f21c7a16489e326e46d4a0a8bc9636c
testingb101 <fakeacccount45677@gmail.com>
2020-01-29 23:09:12 +0500

    loginServlet (with DB connection)  changed to POST method added [Sprint 1]

--- src/java/com/loginServlet.java
+++ src/java/com/loginServlet.java
@@ -80,7 +80,7 @@ public class loginServlet extends HttpServlet {
                 } 
                 }
             }else{  //if session already created go back to homepage
-                RequestDispatcher view = request.getRequestDispatcher("home.html");
+                RequestDispatcher view = request.getRequestDispatcher("home.html"); //will change to jsp later
                 view.forward(request, response);
             }
 
