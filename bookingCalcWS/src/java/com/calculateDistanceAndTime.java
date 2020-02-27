b480d43f3c9cd3d83ac3d486d4a55cc8a6473e6a
testingb101 <fakeacccount45677@gmail.com>
2020-02-27 16:46:05 +0500

    added web service (distance & time calculator)  [Sprint 2]

---
+++ bookingCalcWS/src/java/com/calculateDistanceAndTime.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author NUser1
+ */
+@WebService(serviceName = "calculateDistanceAndTime")
+@Stateless()
+public class calculateDistanceAndTime {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "calculateDistanceAndTime")
+    public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
+        //TODO write your implementation code here:        
+        Double bookingfee = 1.5;
+        Double baserate = 0.1;
+        Double drivetime = 0.2;
+        int time = i;
+        int distance = j;
+        int sum = (int) ((baserate*distance)+(drivetime * time) + bookingfee);
+        
+        return sum;
+    }
+}
